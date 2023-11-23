16427f7f691197dcc5e814cd68f08167bf8c611f
xFlameLiq <benjamincore2002@hotmail.com>
2023-11-22 18:23:04 -0600

    Ready To Modify

---
+++ src/pages/LoginMUI.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material";
+
+const LoginMUI = () => {
+  return (
+    <>
+      <Button variant="contained" color="success">
+        Success
+      </Button>
+    </>
+  );
+};
+
+export default LoginMUI;
